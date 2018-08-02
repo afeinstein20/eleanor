928aefa87ee4da521da7746f72c3d694a74224b4
Adina Feinstein <adinadfeinstein@gmail.com>
2018-08-02 11:41:35 -0500

    testing visualize

--- ELLIE_v1.2/test.py
+++ ELLIE_v1.2/test.py
@@ -1,11 +1,14 @@
 from customTPFs import custom_tpf as ctpf
-
+from customTPFs import visualize as vis
 
 ex = ctpf()
 ex.pos=[250,48]
 ex.multiFile='testTIC.txt'
 
-match = ex.crossmatch_by_position(r=0.01, service='Mast.Tic.Crossmatch')
-print(match)
-multi = ex.crossmatch_multi_to_gaia()
-print(multi)
+#match = ex.crossmatch_by_position(r=0.01, service='Mast.Tic.Crossmatch')
+#print(match)
+#multi = ex.crossmatch_multi_to_gaia()
+#print(multi)
+
+test = vis(219870537)
+lc = test.tpf_movie(cmap='inferno')
