049eb2ae7fce96e91f37cc0e5cce6c62f76c799a
Adina Feinstein <adinadfeinstein@gmail.com>
2018-08-10 10:42:56 -0500

    additions
    Added class for systematics corrections, creating a normalized light curve, creating post cards, and creating the pointing model

--- ELLIE_v1.2/customTPFs.py
+++ ELLIE_v1.2/customTPFs.py
@@ -7,7 +7,7 @@ import matplotlib
 import astropy.units as u
 from astropy.coordinates import SkyCoord
 from astroquery.vizier import Vizier
-from astroquery.gaia import Gaia
+#from astroquery.gaia import Gaia
 from astroquery.mast import Catalogs
 from astropy.io import fits
 from astropy.table import Table
@@ -21,7 +21,9 @@ import matplotlib.gridspec as gridspec
 
 from photutils import CircularAperture, RectangularAperture, aperture_photometry
 from lightkurve import KeplerTargetPixelFile as ktpf
+from lightkurve import SFFCorrector
 from scipy import ndimage
+from scipy.optimize import minimize
 import collections
 
 try: # Python 3.x
@@ -44,7 +46,7 @@ class custom_tpf:
         multiFile: Filename with either a list of TIC IDs, Gaia IDs, or RA,Dec position pairs (default:None)
     """
 
-    def __init__(self, tic=None, gaia=None, pos=None, multiFile=None):
+    def __init__(self, tic=None, gaia=None, pos=None, multiFile=None, dir=None, camera=None, chip=None):
         """ USER INPUTS """
         if tic == None:
             self.tic = 0
@@ -62,6 +64,18 @@ class custom_tpf:
             self.mutliFile = []
         else:
             self.multiFile = multiFile
+        if dir == None:
+            self.dir = ''
+        else:
+            self.dir = dir
+        if camera==None:
+            self.camera=0
+        else:
+            self.camera=camera
+        if chip==None:
+            self.chip=0
+        else:
+            self.chip=chip
 
 
 
@@ -191,6 +205,8 @@ class custom_tpf:
         ---------- 
             source_id, ra, dec, gmag, pmra, pmdec, parallax
         """
+        from astroquery.gaia import Gaia
+
         if multiSource != None:
             source = multiSource
         elif self.gaia == 0:
@@ -323,10 +339,35 @@ class custom_tpf:
         t.remove_row(0)
         return t
 
+    def tic_by_contamination(self, pos, r, contam):
+        """
+        Allows the user to perform a counts only query or get the usual grid of results. When unsure
+        how many results is expcted, it is best to first perform a counts query to avoid memory overflow
+        Parameters
+        ----------
+            pos: [RA,Dec] pair to be the center of the search
+            r: radius of cone search
+            contam: [min,max] list of how much allowed contamination
+        Returns
+        ----------
+            json.loads(outString): a table of source(s) in radius
+        """
+        request = {'service':'Mast.Catalogs.Filtered.Tic.Position',
+               'format':'json',
+               'params': {'columns':'c.*',
+                          'filters': [{'paramName':'contratio',
+                                       'values':[{'min':contam[0], 'max':contam[1]}]}],
+                          'ra':pos[0],
+                          'dec':pos[1],
+                          'radius':r
+                          }}
+        headers, outString = self.mastQuery(request)
+        return self.jsonTable(json.loads(outString))
+
     
     def pointing_model(self):
         
-        def sortByDate(fns, dir):
+        def sort_by_date(fns, dir):
             """
             Sort FITS files by start date of observation
             Parameters
@@ -363,10 +404,10 @@ class custom_tpf:
             return np.min(dist)
 
 
-        def findIsolated(x, y):
+        def find_isolated(x, y):
             """
             Find isolated sources in the image where an isolated source is 
-            >= 5.5 pixels away from any other source
+            >= 8.0 pixels away from any other source
             Parameters
             ---------- 
                 x: a list of x-coords for all sources
@@ -378,12 +419,83 @@ class custom_tpf:
             isolated = []
             for i in range(len(x)):
                 dist = nearest(x[i], y[i], x, y)
-                if dist >= 5.5:
+                if dist >= 8.0:
                     isolated.append(i)
             return isolated
 
+        def calc_shift(dir, fns, x, y, corrFile, mast):
+            """
+            Calculates the deltaX, deltaY, and deltaTheta (rotation) for isolated 
+            sources in order to create a pointing model for the entire chip
+            Parameters
+            ---------- 
+                dir: directory FITS files are in
+                fns: all FITS filenames
+                x  : a list of x-coordinates for isolated sources
+                y  : a list of y-coordinates for isolated sources
+                corrFile: name of file to write corrections to for each cadence
+                mast: data for first cadence (for reference)
+            Returns
+            ---------- 
+            """
+            
+            # Fits the best deltaX, deltaY, deltaT
+            def model(params):
+                nonlocal x, y, centroids
+                theta, xT, yT = params
+                xRot = x*np.cos(theta) - y*np.sin(theta) + xT
+                yRot = x*np.sin(theta) + y*np.cos(theta) + yT
+                dist = np.sqrt( (xRot-centroids[:,0])**2 + (yRot-centroids[:,1])**2 )
+                return np.sum( np.square(dist) )
+            fns = [dir+i for i in fns]
+            
+            # Shifts file to center at (0,0)
+            x, y = x-len(mast)/2, y-len(mast[0])/2
+            
+            with open(corrFile, 'w') as tf:
+                tf.write('cadence, medT, medX, medY\n')
+                
+            matrix = np.zeros((len(x), len(fns), 2))
 
+            for i in range(len(x)):
+                tpf = ktpf.from_fits_images(images=fns, position=(x[i], y[i]), size=(6,6))
+                #tpf.to_fits(output_fn='{}_tpf.fits'.format(i))
+                for j in range(len(fns)):
+                    com = ndimage.measurements.center_of_mass(tpf.flux[j].T-np.median(tpf.flux[j])) # subtracts background
+                    matrix[i][j][0] = com[0]+x[i]
+                    matrix[i][j][1] = com[1]+y[i]
+            for i in range(len(x)):
+                centroids = matrix[:,i]
+                if i == 0:
+                    initGuess = [0.001, 0.1, -0.1]
+                else:
+                    initGuess = solution.x
+                bnds = ((-0.08, 0.08), (-5.0, 5.0), (-5.0, 5.0))
+                solution = minimize(model, initGuess, method='L-BFGS-B', bounds=bnds, options={'ftol':5e-11,
+                                                                                               'gtol':5e-05})
+                with open(corrFile, 'a') as tf:
+                    tf.write('{}\n'.format(str(i) + ' ' + ' '.join(str(e) for e in success.x)))
+            return
         
+        filnames = np.array(os.listdir(self.dir))
+        fitsInds = np.array([i for i,item in enumerate(filenames) if "fits" in item])
+        filenames = sort_by_date(filenames[fitsInds], dir)
+        
+        mast, header = fits.getdata(self.dir, filenames[0])
+        pos = [header['CRVAL1'], header['CRVAL2']]
+        dataTable = self.tic_by_contamination(pos, 6*np.sqrt(2), [0.0, 5e-4])
+        ra, dec = dataTable['ra'], dataTable['dec']
+        xy = WCS(header).all_world2pix(ra,dec,1)
+
+        in_frame = np.where((y>=44.) & (y<len(mast)-45.) & (x>=0.) & (x<len(mast[0])-41.))[0]
+        x, y = xy[0][in_frame], xy[1][in_frame]
+        
+        isolated = find_isolated(x,y)
+        x, y = x[isolated], y[isolated]
+        calcShift(dir, filenames, x, y, 'pointingModel_{}-{}.txt'.format(self.camera, self.chip), mast)
+        return
+
+    
     def download_tic_tpf(self, custom=False):
         """
         This function finds the sector, camera, and chip a target is located in
@@ -401,6 +513,168 @@ class custom_tpf:
         return
 
 
+    def make_postcard(self):
+        """
+        Creates a "postcard" of (350,350) pixels and stacks all cadences in this one file
+        Allows the user to easily create multiple individual TPFs when sources are in the same region
+        Parameters
+        ----------
+        Returns
+        ----------
+        """
+        fns = np.array(os.listdir(self.dir))
+        fns = fns[np.array([i for i,item in enumerate(fns) if 'fits' in item])]
+        fns = [dir+i for i in fns]
+        
+        mast, header = fits.getdata(fns[0], header=True)
+
+        x, y = np.linspace(0, len(mast), 4, dtype=int), np.linspace(0, len(mast[0]), 4, dtype=int)
+        x_cens, y_cens = [], []
+        
+        cat = 'postcard_catalog.txt'
+        with open(Cat, 'w') as tf:
+            tf.write('filename ra_low ra_up dec_low dec_up\n')
+        for i in range(len(x)-1):
+            for j in range(len(y)-1):
+                 fn = 'postcard_{}-{}.fits'.format(i, j)
+                 x_cen = (x[i]+x[i+1]) / 2.
+                 y_cen = (y[j]+y[j+1]) / 2.
+                 x_cens.append(x_cen)
+                 y_cens.append(y_cen)
+                 
+                 # Converts (RA,Dec) -> (x,y) and creates TPF for postcard
+                 radec = WCS(mheader).all_pix2world(x_cen, y_cen, 1)
+                 tpf = ktpf.from_fits_images(images=fns, position=(x_cen,y_cen), size=(350,350))
+                 tpf.to_fits(output_fn=fn)
+
+                 # Edits header of fits file for additional information
+                 fits.setval(fn, 'CEN_X' , value=np.round(x_cen,5))
+                 fits.setval(fn, 'CEN_Y' , value=np.round(y_cen,5))
+                 fits.setval(fn, 'CEN_RA', value=float(radec[0]))
+                 fits.setval(fn, 'CEN_DEC', value=float(radec[1]))
+
+                 # Adds to catalog of postcard information
+                 lower = WCS(mheader).all_pix2world(x_cen-350, y_cen-350, 1)
+                 upper = WCS(mheader).all_pix2world(x_cen+350, y_cen+350, 1)
+                 row = [fn, lower[0], lower[1], upper[0], upper[1]]
+                 with open(cat, 'a') as tf:
+                     tf.write('{}\n'.format(' '.join(str(e) for e in row)))
+        return
+    
+
+class create_lightcurve:
+    """
+    Allows the user to create their own light curve. This includes:
+    1. Correcting for systematics & choosing which systematics to correct for (jitter vs. roll)
+    2. Choosing a custom aperture for the source
+    """
+    def __init__(self, id, dir=None, camera=None, chip=None):
+        self.id     = id
+        self.camera = camera
+        self.chip   = chip
+        if dir==None:
+            self.tpf_file = '{}_tpf.fits'.format(self.id)
+        else:
+            self.tpf_file = dir + '{}_tpf.fits'.format(self.id)
+            
+
+    def custom_aperture(self):
+        return
+
+
+    def lightcurve(self):
+        """
+        Loads in source TPF and pointing model to create a custom light curve that tracks
+        the pointing model/center of mass of the source
+        Parameters
+        ---------- 
+        Returns
+        ---------- 
+            lc / np.nanmedian(lc): normalized lightcurve
+            x_point: x-coordinate of source after pointing model correction
+            y_point: y-coordinate of source after pointing model correction
+        """
+        theta, delX, delY = np.loadtxt('pointingModel_{}-{}.txt'.format(self.camera, self.chip),
+                                       skiprows=1, usecls=(1,2,3), unpack=True)
+        tpf = ktpf.from_fits(self.tpf_file)
+        hdu = fits.open(self.tpf_file)
+        
+        cen_x, cen_y = len(tpf.flux[0])/2., len(tpf.flux[0])/2.
+        
+        # Creates estimated center location taking the pointing model into account
+        x_point = cen_x * np.cos(np.radians(theta)) - cen_y * np.sin(np.radians(theta)) - delx
+        y_point = cen_x * np.sin(np.radians(theta)) + cen_y * np.cos(np.radians(theta)) - dely
+        
+        # Creates the light curve given different center locations per cadence
+        lc = []
+        aperture = self.custom_aperture()
+        for f in range(len(tpf.flux)):
+            pos = [x_point[f], y_point[f]]
+            lc.append(aperture_photometry(tpf.flux[f], aperture)['aperture_sum'].data[0])
+        return np.array(lc / np.nanmedian(lc)), x_point, y_point
+
+        
+    def jitter_correction(self):
+        """
+        Corrects for systematic errors stemming from jitter of the telescope
+        Jitter correction method taken from Knutson et al. (2008) https://arxiv.org/pdf/0709.3984.pdf
+        Parameters
+        ----------
+        Returns
+        ----------
+            lc: normalized & corrected light curve
+        """
+
+        def parabola(params, x, y, f_obs, y_err):
+            """ Used to minimize systematics from jitter """
+            c1, c2, c3, c4, c5 = params
+            f_corr = f_obs * (c1 + c2*(x-2.5) + c3*(x-2.5)**2 + c4*(y-2.5) + c5*(y-2.5)**2)
+            return np.sum( ( (1-f_corr)/y_err)**2)
+
+
+        lc_norm, x_point, y_point = self.lightcurve()
+        
+        # Masks out anything >= 2.5 sigma above the mean
+        mask = np.ones(len(lc), dtype=bool)
+        for i in range(5):
+            lc_new = []
+            std_mask = np.std(lc[mask])
+            inds = np.where(lc <= np.mean(lc)-2.5*std_mask)
+            y_err = np.ones(len(lc))*np.std(lc)
+
+            for j in inds:
+                y_err[j] = np.inf
+                mask[j]  = False
+
+            if i == 0:
+                initGuess = [3, 3, 3, 3, 3]
+            else:
+                initGuess = test.x
+
+            bnds = ((-15.,15.), (-15.,15.), (-15.,15.), (-15.,15.), (-15.,15.))
+            test = minimize(parabola, initGuess, args=(x_point, y_point, lc, y_err), bounds=bnds)
+            
+            c1, c2, c3, c4, c5 = test.x
+            lc_new = lc * (c1 + c2*(x_point-2.5) + c3*(x_point-2.5)**2 + c4*(y_point-2.5) + c5*(y_point-2.5)**2)
+        return lc_new
+
+    
+    def roll_correction(self):
+        """
+        Corrects for light curve systematics associated with roll of the telescope
+        Taken from Vanderburg & Johnson (2014) https://arxiv.org/pdf/1408.3853.pdf as implemented by lightkurve 
+        ----------
+           lc: normalized & corrected light curve
+        """
+        lc_nrom, x_point, y_point = self.lightcurve()
+        time = np.arange(0, len(lc), 1)
+        
+        sff = SFFCorrector()
+        lc_corrected = sff.correct(time, lc, x_point, y_point, niters=1, windows=1, polyorder=5)
+        long_term_trend = sff.trend
+        return lc_corrected.flux*long_term_trend
+
+        
 
 class visualize:
     """
