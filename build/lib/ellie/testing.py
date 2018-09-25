61f44a145f5aab5e4b6f5c3fa6cd2d7e2b95e594
Adina Feinstein <adina.d.feinstein@gmail.com>
2018-09-25 14:28:20 -0500

    no changed here

--- build/lib/ellie/testing.py
+++ build/lib/ellie/testing.py
@@ -1,15 +1,14 @@
-from ellie import data_products
-from ellie import find_sources
-from ellie import visualize
-from astropy.io import fits
-import numpy as np
-import matplotlib.pyplot as plt
-from bokeh.plotting import show
+#from postcard import postcard
+#a = postcard(sector=1, camera=2, chip=1)
+#a = postcard(post_name='postcard_1_3-4_2-0.fits')
+#a.grab()
+#print(a.local_path)
+#a.read()
+#print(a.xy_center)
 
 
-vis = visualize(tic=193945542)
-#ani = vis.tpf_movie(aperture=True, plot_lc=True, com=True)
-#plt.show()
-lc = vis.click_aperture()
-plt.plot(np.arange(0,len(lc),1), lc, 'k')
-plt.show()
+from ffi import ffi
+a = ffi(sector=1, camera=3, chip=1)
+a.download_ffis()
+a.sort_by_date()
+a.pointing_model_per_cadence()
