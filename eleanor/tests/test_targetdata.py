0e1be91c043fb2cc87ed71c3b8e4a83ae5fa804a
benmontet <bmontet@uchicago.edu>
2018-12-15 12:47:30 -0600

    fix in test

--- eleanor/tests/test_targetdata.py
+++ eleanor/tests/test_targetdata.py
@@ -29,5 +29,5 @@ def test_making_tpfs():
 def test_arb_size_tpfs():
     star = Source(tic=29987116)
     data = TargetData(star, height=15, width=12)
-    assert(np.shape(data.raw_flux[0] == (15,12)))
+    assert(np.shape(data.raw_flux[0] == (15,13))) # eleanor enforces oddness
 '''
