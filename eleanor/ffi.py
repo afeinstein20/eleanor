2e21229a7116e2f63d309b8ab7ef887f3f2cfe85
Adina Feinstein <adina.d.feinstein@gmail.com>
2019-01-03 10:59:17 -0600

    changed pm link

--- eleanor/ffi.py
+++ eleanor/ffi.py
@@ -19,8 +19,9 @@ def load_pointing_model(sector, camera, chip):
     """ Loads in pointing model from website.
     """
     sector = int(sector)
-    pointing_link = urllib.request.urlopen('https://archipelago.uchicago.edu/tess_postcards/pointingModel_{0:04d}_{1}-{2}.txt'
-                                           ''.format(sector, camera, chip))
+    pointing_link = urllib.request.urlopen('https://users.flatironinstitute.org/dforeman/public_www/tess/postcards_test/s{0:04d}/pointing_model/pointingModel_{0:04d}_{1}-{2}.txt'.format(sector,
+                                                                                                                                                                                          camera,
+                                                                                                                                                                                          chip))
     pointing = pointing_link.read().decode('utf-8')
     pointing = Table.read(pointing, format='ascii.basic') # guide to postcard locations
     return pointing
