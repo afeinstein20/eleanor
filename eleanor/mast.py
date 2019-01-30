ae6b021e1b6805b9d2886c4ba28c4550b5505ee7
Adina Feinstein <adina.d.feinstein@gmail.com>
2019-01-29 18:16:37 -0600

    returns TIC version

--- eleanor/mast.py
+++ eleanor/mast.py
@@ -149,7 +149,7 @@ def coords_from_tic(tic):
     """
 
     ticData = Catalogs.query_object('tic'+str(tic), radius=.0001, catalog="TIC")
-    return [ticData['ra'].data[0], ticData['dec'].data[0]], ticData['Tmag'].data
+    return [ticData['ra'].data[0], ticData['dec'].data[0]], ticData['Tmag'].data, ticData['version']
 
 def coords_from_gaia(gaia_id):
     """Returns table of Gaia DR2 data given a source_id."""
@@ -167,7 +167,7 @@ def tic_from_coords(coords):
     tess = crossmatch_by_position(coords, 0.01, 'Mast.Tic.Crossmatch')
     tessPos = [tess['MatchRA'], tess['MatchDEC']]
     sepTess = crossmatch_distance(coords, tessPos)
-    return int(tess[sepTess==np.min(sepTess)]['MatchID'].data[0]), [tess[sepTess==np.min(sepTess)]['Tmag'].data[0]], sepTess[sepTess==np.min(sepTess)]/u.arcsec
+    return int(tess[sepTess==np.min(sepTess)]['MatchID'].data[0]), [tess[sepTess==np.min(sepTess)]['Tmag'].data[0]], sepTess[sepTess==np.min(sepTess)]/u.arcsec, tess['version'][0]
 
 def gaia_from_coords(coords):
     """Returns Gaia ID of best match(es) to input coords."""
