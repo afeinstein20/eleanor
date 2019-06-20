da1507d1fe8afb9b92ec53826ef447542bfd2cfc
benmontet <bmontet@uchicago.edu>
2019-06-20 09:28:57 -0500

    q11 ready to go except for cbvs

--- eleanor/source.py
+++ eleanor/source.py
@@ -48,7 +48,7 @@ def multi_sectors(sectors, tic=None, gaia=None, coords=None, tc=False):
             if type(coords) is SkyCoord:
                 coords = (coords.ra.degree, coords.dec.degree)
             result = tess_stars2px(8675309, coords[0], coords[1])
-            sector = result[3][result[3] < 10.5]
+            sector = result[3][result[3] < 11.5]
             sectors = sector.tolist()
         print('Found star in Sector(s) ' +" ".join(str(x) for x in sectors))
     if type(sectors) == list:
