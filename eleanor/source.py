c75352c17fea8ff2c480af635f137c11c2937559
Adina Feinstein <adina.d.feinstein@gmail.com>
2019-09-11 09:11:48 -0500

    return SearchError when tess-point returns sector = -1

--- eleanor/source.py
+++ eleanor/source.py
@@ -53,7 +53,11 @@ def multi_sectors(sectors, tic=None, gaia=None, coords=None, tc=False):
             result = tess_stars2px(8675309, coords[0], coords[1])
             sector = result[3][result[3] < 13.5]
             sectors = sector.tolist()
-        print('Found star in Sector(s) ' +" ".join(str(x) for x in sectors))
+
+        if sectors[0] < 0:
+            raise SearchError("Your target is not observed by TESS.")
+        else:
+            print('Found star in Sector(s) ' +" ".join(str(x) for x in sectors))
 
     if (type(sectors) == list) or (type(sectors) == np.ndarray):
         for s in sectors:
