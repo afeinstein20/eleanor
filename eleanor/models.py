aa1aa2c5dbdcfa99a3061c9c3df210c4832c856d
Aditya Sengupta <aditya.sengupta@berkeley.edu>
2020-09-08 14:11:40 -0700

    added zernike model

--- eleanor/models.py
+++ eleanor/models.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 
 class Model:
     """
-    Pretty dumb Gaussian model.
+    Base PSF-fitting model.
     Attributes
     ----------
     shape : tuple
@@ -24,16 +24,15 @@ class Model:
         self.row_ref = row_ref
         self._init_grid()
 
+    def __call__(self, *params):
+        return self.evaluate(*params)
+
     def _init_grid(self):
         r, c = self.row_ref, self.col_ref
         s1, s2 = self.shape
         self.y, self.x = np.mgrid[r:r+s1-1:1j*s1, c:c+s2-1:1j*s2]
 
-
 class Gaussian(Model):
-    def __call__(self, *params):
-        return self.evaluate(*params)
-
     def evaluate(self, flux, xo, yo, a, b, c):
         """
         Evaluate the Gaussian model
@@ -41,7 +40,7 @@ class Gaussian(Model):
         ----------
         flux : tf.Variable
         xo, yo : tf.Variable, tf.Variable
-            Center coordiantes of the Gaussian.
+            Center coordinates of the Gaussian.
         a, b, c : tf.Variable, tf.Variable
             Parameters that control the rotation angle
             and the stretch along the major axis of the Gaussian,
@@ -58,9 +57,6 @@ class Gaussian(Model):
 
 
 class Moffat(Model):
-    def __call__(self, *params):
-        return self.evaluate(*params)
-
     def evaluate(self, flux, xo, yo, a, b, c, beta):
         dx = self.x - xo
         dy = self.y - yo
@@ -68,3 +64,63 @@ class Moffat(Model):
         psf_sum = tf.reduce_sum(psf)
         return flux * psf / psf_sum
 
+class Zernike(Model):
+    '''
+    Use the first 'n_modes' Zernike polynomials with weights given by 'weights'.
+    question for myself for later: what's the preferred datapath for caching these results? ~/.eleanor/...
+    '''
+    def zernike_radial(n, m, r):
+        '''
+        Radial component of the (n, m)th Zernike polynomial over radial coordinates r.
+        Adapted from the 'hcipy' package, https://github.com/ehpor/hcipy/blob/master/hcipy/mode_basis/zernike.py. 
+        TODO proper citation of hcipy if this ends up getting used
+        '''
+        m = abs(m)
+
+        if n == m:
+            res = r**n
+        elif (n - m) == 2:
+            z1 = zernike_radial(n, n, r, cache)
+            z2 = zernike_radial(n - 2, n - 2, r, cache)
+
+            res = n * z1 - (n - 1) * z2
+        else:
+            p = n
+            q = m + 4
+
+            h3 = -4 * (q - 2) * (q - 3) / float((p + q - 2) * (p - q + 4))
+            h2 = h3 * (p + q) * (p - q + 2) / float(4 * (q - 1)) + (q - 2)
+            h1 = q * (q - 1) / 2.0 - q * h2 + h3 * (p + q + 2) * (p - q) / 8.0
+
+            r2 = zernike_radial(2, 2, r, cache)
+            res = h1 * zernike_radial(p, q, r, cache) + (h2 + h3 / r2) * zernike_radial(n, q - 2, r, cache)
+
+        return res
+
+    def zernike_azimuthal(m, theta):
+        if m < 0:
+            res = np.sqrt(2) * np.sin(-m * theta)
+        elif m == 0:
+            return 1
+        else:
+            res = np.sqrt(2) * np.cos(m * theta)
+
+        return res
+
+    def zernike(i):
+        '''
+        Evaluates the 'i'th Zernike polynomial over (self.x, self.y).
+        Adapted from https://github.com/ehpor/hcipy/blob/master/hcipy/mode_basis/zernike.py. 
+        '''
+        n = int((np.sqrt(8 * i + 1) - 1) / 2)
+        r, theta = tf.math.sqrt(self.x ** 2 + self.y ** 2), tf.math.tan(self.y / self.x)
+        return np.sqrt(n + 1) * zernike_azimuthal(m, theta) * zernike_radial(n, m, r)
+
+    def evaluate(self, flux, weights, n_modes=10):
+        '''
+        Evaluates a weighted sum of Zernike polynomials.
+        xo and yo are excluded, because ideally those are fit by tip and tilt.
+        '''
+        psf = sum([zernike(i) * weights[i] for i in range(n_modes)])
+        psf_sum = tf.reduce_sum(psf)
+        return flux * psf / psf_sum
