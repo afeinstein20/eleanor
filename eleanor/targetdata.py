5a3d8c54e3e775214380dc20aee8b3dfda19871c
Adina Feinstein <adina.d.feinstein@gmail.com>
2020-09-23 13:04:04 -0500

    for bright bois

--- eleanor/targetdata.py
+++ eleanor/targetdata.py
@@ -647,11 +647,17 @@ class TargetData(object):
                 stds_2d[ap_size > 8] = 1.0
 
 
-        if self.source_info.tess_mag < 8.5:
-            tpf_stds[ap_size < 8] = 1.0
-            pc_stds[ap_size < 8]  = 1.0
+        if self.source_info.tess_mag < 10.5:
+            tpf_stds[ap_size < 10] = 1.0
+            pc_stds[ap_size < 10]  = 1.0
             if self.source_info.tc == False:
-                stds_2d[ap_size < 8] = 1.0
+                stds_2d[ap_size < 10] = 1.0
+
+        if self.source_info.tess_mag < 7:
+            tpf_stds[ap_size < 20] = 1.0
+            pc_stds[ap_size < 20]  = 1.0
+            if self.source_info.tc == False:
+                stds_2d[ap_size < 20] = 1.0
 
         best_ind_tpf = np.where(tpf_stds == np.nanmin(tpf_stds))[0][0]
         best_ind_pc  = np.where(pc_stds == np.nanmin(pc_stds))[0][0]
