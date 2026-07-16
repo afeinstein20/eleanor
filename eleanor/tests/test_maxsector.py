aa02b7fd1698fbbcf9f9f730efd6b244ff0ff801
Adina Feinstein <adina.d.feinstein@gmail.com>
2026-07-16 12:09:58 -0400

    syntax error in test_maxsector.py

--- eleanor/tests/test_maxsector.py
+++ eleanor/tests/test_maxsector.py
@@ -7,6 +7,6 @@ def test_max_sector():
     assert(mx, 105)
 
 def test_future_max_sector():
-    test_date = datetime.datetime(2027, 1, 19):
+    test_date = datetime.datetime(2027, 1, 19)
     mx = max_sector(test_date)
     assert(mx, 112)
